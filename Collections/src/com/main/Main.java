6fdd63ed7b3eee354421a88b5bdbd30f6d78d8e8
samreenanargis <samreenanargis@samreenas-mbp.local>
2018-04-09 13:46:28 -0400

    random checkin

--- Collections/src/com/main/Main.java
+++ Collections/src/com/main/Main.java
@@ -33,63 +33,13 @@ public class Main {
 	
 	
 	/**
-	 * This method creates a variable of type linkedHashSet.
-	 * It also creates an array 'assAtOnce'
-	 * Then using the addAll() method it adds the values of the array to the linkedHashSet.
-	 * Next it calls the method printSet() of the SetUtilities class which prints all the items in the linkedHashSet.
-	 * @param setUtils
+	 * This method declares an ArrayList of String type. 
+	 * It then adds values to the ArrayList.
+	 * Next it creates an instance of class listUtilities and calls the printList() method which prints 
+	 * the arrayList.
 	 */
-	public static void testLinkedHashSet(SetUtilities setUtils) {	
-		Set<String> linkedHashSet = new LinkedHashSet<String>();
-		String addAtOnce[] = {"One","Two","Three","Four"};
-		linkedHashSet.addAll(Arrays.asList(addAtOnce));	
-		setUtils.printSet(linkedHashSet);
-	}
-	
-	
-	/**
-	 * 
-	 * @param setUtil
-	 */
-	public static void testHashSet(SetUtilities setUtil) {
-		
-		/* Create an instance of Hash set for Hash set example.*/
-		Set<String> test = new HashSet<String>();
-		
-		/* Adding values into Hash set, making sure i add a duplicate value to see if it gets added. */
-		test.add("Samreena");
-		test.add("Praveen");
-		test.add("Samreena");
-		test.add("Naveen");
-		test.add("Soody");
-		test.add("Hasthra");
-		
-		setUtil.printSet(test);
-	}
-	
-	
-	/**
-	 * This method creates a variable of type TreeSet.
-	 * Next it adds values to the variable.
-	 * Finally it calls upon the printSet() method that prints the values added to the variable. 
-	 * @param setUtil
-	 */
-	public static void testTreeSet(SetUtilities setUtil) {
-		
-		Set<String> treeSet = new TreeSet<String>();
-		
-		treeSet.add("Apple");
-		treeSet.add("Dog");
-		treeSet.add("Cat");
-		treeSet.add("Ball");
-		
-		setUtil.printSet(treeSet);
-	}
-	
 	public static void testArrayList() {
-		
 		List<String> arrayList = new ArrayList<String>();
-		
 		arrayList.add("one");
 		arrayList.add("two");
 		arrayList.add("three");
@@ -97,19 +47,15 @@ public class Main {
 		arrayList.add("five");
 		
 		listUtilities listUtils = new listUtilities();
-		listUtils.printList(arrayList);
-		
+		listUtils.printList(arrayList);	
 	}
 	
-	/*public static List combineArrays(String[] array1, String[] array2) {
-		List<String> assorted = new ArrayList<String>(Arrays.asList(array1));
-		assorted.addAll(Arrays.asList(array2));
-		return assorted;
-	}*/
 	
 	
 	/**
-	 * 
+	 * This method accepts a List and a String array for arguments. 
+	 * First the method converts the String array to a List. 
+	 * Then it creates a variable of type Iterator and assigns the list to it. 
 	 * @param list
 	 * @param array2
 	 */
@@ -132,5 +78,73 @@ public class Main {
 		}
 		System.out.println("Items remaining: "+list);
 	}
+	
+	
+	
+	/**
+	 * 
+	 * @param setUtil
+	 */
+	public static void testHashSet(SetUtilities setUtil) {
+		
+		/* Declare a HashSet for HashSet example.*/
+		Set<String> test = new HashSet<String>();
+		
+		/* Adding values into Hash set, making sure i add a duplicate value to see if it gets added. */
+		test.add("Samreena");
+		test.add("Praveen");
+		test.add("Samreena");
+		test.add("Naveen");
+		test.add("Soody");
+		test.add("Hasthra");
+
+		setUtil.printSet(test);
+	}
+	
+	
+	/**
+	 * This method creates a variable of type linkedHashSet.
+	 * It also creates an array 'addAtOnce'
+	 * Then using the addAll() method it adds the values of the array to the linkedHashSet.
+	 * Next it calls the method printSet() of the SetUtilities class which prints all the items in the linkedHashSet.
+	 * @param setUtils
+	 */
+	public static void testLinkedHashSet(SetUtilities setUtils) {	
+		Set<String> linkedHashSet = new LinkedHashSet<String>();
+		String addAtOnce[] = {"One","Two","Three","Four"};
+		linkedHashSet.addAll(Arrays.asList(addAtOnce));	
+		setUtils.printSet(linkedHashSet);
+	}
+	
+	
+	
+	/**
+	 * This method creates a variable of type TreeSet.
+	 * Next it adds values to the variable.
+	 * Finally it calls upon the printSet() method that prints the values added to the variable. 
+	 * @param setUtil
+	 */
+	public static void testTreeSet(SetUtilities setUtil) {
+		
+		Set<String> treeSet = new TreeSet<String>();
+		
+		treeSet.add("Apple");
+		treeSet.add("Dog");
+		treeSet.add("Cat");
+		treeSet.add("Ball");
+		
+		setUtil.printSet(treeSet);
+	}
+	
+	
+	
+	/*public static List combineArrays(String[] array1, String[] array2) {
+		List<String> assorted = new ArrayList<String>(Arrays.asList(array1));
+		assorted.addAll(Arrays.asList(array2));
+		return assorted;
+	}*/
+	
+	
+
 
 }
